80801503e2029e640626f8655d1955ba656a6c12
Gauhar Nawab <gauharnawab20@gmail.com>
2023-09-16 14:59:00 +0530

    Added

--- src/com/leetcode/Array/MajorityElement.java
+++ src/com/leetcode/Array/MajorityElement.java
@@ -13,7 +13,10 @@ public class MajorityElement {
 
     public static void main(String[] args) {
         int[] arr = {1,2,1};
+        int[] arr1 = {1,2,2,3,1,2};
         int result = findMajority(arr);
+        int result1 = findMajority(arr1);
         System.out.println(result);
+        System.out.println(result1);
     }
 }
