ce0f6d2f596fef4ec0ea5cd44a8ae4297d92843d
Gauhar Nawab <gauharnawab20@gmail.com>
2023-12-08 20:19:33 +0530

    Day-6 December leetcode

---
+++ src/december2023_leetcode/TreeNode.java
@@ -0,0 +1,67 @@
+package december2023_leetcode;
+
+//606. Construct String from Binary Tree
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+      this.val = x;
+    }
+}
+
+  class Solution {
+    public String tree2str(TreeNode root) {
+        if (root == null) {
+            return "";
+        }
+
+        // Step 1: Start with an empty result string
+        StringBuilder result = new StringBuilder();
+
+        // Step 2: Perform preorder traversal
+        preorderTraversal(root, result);
+
+        // Step 3: Return the final result string
+        return result.toString();
+    }
+
+    private void preorderTraversal(TreeNode node, StringBuilder result) {
+        if (node == null) {
+            return;
+        }
+
+        // Step 4: Append the current node's value to the result
+        result.append(node.val);
+
+        // Step 5: Check if the current node has a left child or a right child
+        if (node.left != null || node.right != null) {
+
+            // Step 6: If there is a left child, add empty parentheses for it
+            result.append("(");
+            preorderTraversal(node.left, result);
+            result.append(")");
+        }
+
+        // Step 7: If there is a right child, process it similarly
+        if (node.right != null) {
+            result.append("(");
+            preorderTraversal(node.right, result);
+            result.append(")");
+        }
+
+        // Step 8: The recursion will handle all the child nodes
+    }
+
+    // Example usage
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+
+        String result = solution.tree2str(root);
+        System.out.println(result);
+    }
+}
